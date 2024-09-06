758ec9a6b6e3eb2a296aa6c2fee0f5937545c13d
Yamabooks <ok210200@oks.iput.ac.jp>
2024-09-06 14:42:11 +0900

    音声ファイル変更

--- yamamoto/voicevox/voicevox.py
+++ yamamoto/voicevox/voicevox.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 from voicevox_core import VoicevoxCore, METAS
-import sys, os
 
 core = VoicevoxCore(open_jtalk_dict_dir=Path("open_jtalk_dic_utf_8-1.11"))
-speaker_id = 2
 
-text = sys.argv[1]
-if not core.is_model_loaded(speaker_id):
-    core.load_model(speaker_id)
-wave_bytes = core.tts(text, speaker_id)
-with open("./" + text + ".wav", "wb") as f:
-    f.write(wave_bytes)
+# メタデータ閲覧
+#from pprint import pprint
+#pprint(METAS)
+
+speaker_id = 38
+
+text = "こんにちは、これはテストです。"
+if not core.is_model_loaded(speaker_id):  # モデルが読み込まれていない場合
+    core.load_model(speaker_id)  # 指定したidのモデルを読み込む
+wave_bytes = core.tts(text, speaker_id)  # 音声合成を行う
+with open("output.wav", "wb") as f:
+    f.write(wave_bytes)  # ファイルに書き出す
